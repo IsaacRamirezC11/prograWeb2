2d7da6642d3d73475639b49326ce6b156e9002f2
IsaacRamirezC11 <isaaccortes08@aragon.unam.mx>
2025-05-07 12:05:35 -0600

    Visca barca

---
+++ Clase07052025/routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const router = express.Router();
+
+// Ruta principal
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+// Ruta de registro
+
+router.get("/registro.html", (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/registro.html'));
+  });
+
+
+module.exports = router;
