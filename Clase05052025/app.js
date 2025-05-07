879dc60d104b5219d3c0f0c2fd2dda54d11b1dc8
IsaacRamirezC11 <isaaccortes08@aragon.unam.mx>
2025-05-06 18:07:59 -0600

    Tarea de uso de path y rutas en node

--- Clase05052025/app.js
+++ Clase05052025/app.js
@@ -1,19 +1,24 @@
-//importacion de express
-const express = require('express');
+//Version 3
+const express=require('express');
+const path=require('path');
 
-
-//instancia de express
-const app = express();
+const app=express();
 
 //puerto
-const port = 3000;
+const port=5644;
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo!')
-    res.sendFile("c:/Users/Andres/Documents/Escuela/Programacion-web2/05-05-25/index.html")
+//manejador de la solicitudess
+app.get("/",(req,res)=>{
     
+    // res.send("HolaMundo")
+    
+    //es importante agregar el disco local
+    
+    res.sendFile("C:/Users/Saulr/Desktop/prograWeb2/Clase05052025/index.html");
+    
+    // res.sendFile(path.join(__dirname+"/index.html"));
 });
-
-app.listen(port, () => {
-    console.log('http://127.0.0.1:${port}');
+//salida del servidor
+app.listen(port,()=>{
+    console.log(`http://127.0.0.1:${port}`);
 });
