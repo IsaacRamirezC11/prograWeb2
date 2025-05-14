75b8f635166eb7c725394b9adfd54a6f91df2477
IsaacRamirezC11 <isaaccortes08@aragon.unam.mx>
2025-05-14 12:38:43 -0600

    trabajo 14 de mayo

--- final/app.js
+++ final/app.js
@@ -82,11 +82,11 @@ app.post('/add', (req, res) => {
 
     /*
         Nombre: Isaac
-        Correro: isaaccortes09@aragon.unam.mx
+        Correro: isaaccortes08@aragon.unam.mx
         -->
 
         Nombre: Isaac
-        Correro: isaaccortes09@aragon.unam.mx
+        Correro: isaaccortes08@aragon.unam.mx
 
     */
 
@@ -102,3 +102,81 @@ db.query(insertarRegistro, [name, email], (err, results) => {
             }
         });
 });
+
+//Modulo editar un usuario
+app.post('/add', (req, res) => {
+
+});
+
+//editar un usuario
+app.get('/edit/:id', (req, res) => {
+    const{id}=req.params;
+    const buscarUsuariID = 'SELECT * FROM users WHERE id = ?';
+    db.query(buscarUsuariID, [id], (err, results) => {
+        if (err) {
+            console.error('Error al buscar el usuario', err);
+            res.send('Error, no se pudo recuperar la informacion de la base de datos');
+        } else {
+            res.render('edit', { user: results[0] });
+        }
+    });
+});
+
+// update
+
+app.post('/update/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const actualizarUsuario = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+
+    db.query(actualizarUsuario, [name, email, id], (err ) => {
+        if (err) {
+            console.error('Error al actualizar el usuario', err);
+            res.send('Error, no se pudo actualizar el usuario');
+        } else {
+            console.log('Usuario actualizado correctamente');
+            res.redirect('/');
+        }
+    });
+
+});
+
+
+// Eliminar un usuario
+app.get('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const eliminarUsuario = 'DELETE FROM users WHERE id = ?';
+
+    db.query(eliminarUsuario, [id], (err) => {
+        if (err) {
+            console.error('Error al eliminar el usuario', err);
+            res.send('Error, no se pudo eliminar el usuario');
+        } else {
+            console.log('Usuario eliminado correctamente');
+            res.redirect('/');
+        }
+    });
+});
+
+// Eliminar todos los usuarios y reiniciar autoincrement
+app.get('/deleteAll', (req, res) => {
+    db.query('DELETE FROM users', (err) => {
+        if (err) {
+            console.error('Error al eliminar todos los usuarios', err);
+            res.send('Error, no se pudo eliminar los usuarios');
+        } else {
+            db.query('ALTER TABLE users AUTO_INCREMENT = 1', (err2) => {
+                if (err2) {
+                    console.error('Error al reiniciar el autoincrement', err2);
+                    res.send('Usuarios eliminados, pero error al reiniciar autoincrement');
+                } else {
+                    console.log('Todos los usuarios eliminados y autoincrement reiniciado');
+                    res.redirect('/');
+                }
+            });
+        }
+    });
+});
+   
